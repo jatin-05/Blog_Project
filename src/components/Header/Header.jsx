ba6c0e22655ecbcb5d9d27ff6073f825788dd18b
jatin <jatingupta108@gmail.com>
2024-07-12 18:01:19 +0530

    img

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -44,7 +44,7 @@ function Header() {
 
           <div className='mr-4'>
             <Link to='/'>
-              <img src="/logo/white-Logo.png" alt="logo" className='size-12' />
+              <img src="../../../logo/white-Logo.png" alt="logo" className='size-12' />
             </Link>
           </div>
 
